362007c664d1c2eb56057fa9bd70bfee564ee54f
Xuân Yêu Thương <165404404+xuanyethuong@users.noreply.github.com>
2024-06-08 11:13:22 +0700

    Update evaluation.py
    change some contents to english.

--- module 1/week 1/evaluation.py
+++ module 1/week 1/evaluation.py
@@ -1,14 +1,3 @@
-#Viết function thực hiện đánh giá classification model bằng F1-Score
-'''
-• Precision = TP/(TP + FP)
-• Recall = TP/(TP + F N)
-• F1-score = 2 ∗ ((P recision ∗ Recall)/(P recision + Recall))
-• Input: function nhận 3 giá trị tp, fp, fn
-• Output: print ra kết quả của Precision, Recall, và F1-score
-NOTE: Đề bài yêu cầu các điều kiện sau
-• Phải kiểm tra giá trị nhận vào tp, fp, fn là type int, nếu là type khác thì print ví dụ check fn là float, print ’fn must be int’ và thoát hàm hoặc dừng chương trình.
-• Yêu cầu tp, fp, fn phải đều lớn hơn 0, nếu không thì print ’tp and fp and fn must be greater than zero’ và thoát hàm hoặc dừng chương trình
-'''
 def evalute_classification(tp,fp,fn):
     if not isinstance(tp, int):
         print('tp must be int')
@@ -35,17 +24,17 @@ def is_number(value):
         return False
 def sig_func(x):
     sig = 1/(1+math.exp(-x))
-    print (f"Giá trị hàm sigmoid của {x} là: {sig}")
+    print (f"Sigmoid value of {x} is: {sig}")
 def reLU_func(x):
     if x <= 0:
         reLU_value = 0
     reLU_value = x
-    print (f"Giá trị hàm reLU của {x} là: {reLU_value}")
+    print (f"reLU value of {x} is: {reLU_value}")
 def eLU_func(x,alpha):
     if x <= 0:
         eLU_value = alpha*(math.exp(x)-1)
     eLU_value = x
-    print (f"Giá trị hàm eLU của {x} là: {eLU_value}")
+    print (f"eLU value of {x} is: {eLU_value}")
 def activation(x, alpha, act_name):
     if not is_number(x):
         print ("must be a number")
@@ -57,7 +46,7 @@ def activation(x, alpha, act_name):
     elif act_name == 'elu':
         return eLU_func(x,alpha)
     else:
-        raise ValueError("Tên hàm không hợp lệ")
+        raise ValueError(" Name Function is not valid ")
 def calc_ae(y, y_hat):
     return abs(y - y_hat)
 def calc_se(y, y_hat):
@@ -87,10 +76,10 @@ def calc_approx_cosh(x, n):
         cosh_approx += term
     return cosh_approx
 
-### hàm chính 
+### main function
 if __name__ == "__main__":
     evalute_classification(2, 3, 5)
-    # Test hàm activation_function
+    # Test activation_function
     alpha = 2
     activation(3, alpha,'sigmoid')
     activation(1, alpha,'relu')
